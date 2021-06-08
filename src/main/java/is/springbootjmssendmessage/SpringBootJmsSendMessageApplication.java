7bccce41d2ccde02d521bd842b186d5d99add1f1
StepanenkoIS <Citizen(18)>
2021-06-08 19:10:44 +0400

    Create Spring Initializr

---
+++ src/main/java/is/springbootjmssendmessage/SpringBootJmsSendMessageApplication.java
@@ -0,0 +1,13 @@
+package is.springbootjmssendmessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJmsSendMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJmsSendMessageApplication.class, args);
+	}
+
+}
